2db9cf4e4ed421c93df2552e22ac1e758ecfa13c
Josh Bodinet <jbodinet@yahoo.com>
2020-02-05 08:01:22 -0800

    added Event.h

---
+++ OpenALTest/Event.h
@@ -0,0 +1,76 @@
+//
+//  Event.h
+//  OpenALTest
+//
+//  Created by Joshua Bodinet on 2/3/20.
+//  Copyright © 2020 JoshuaBodinet. All rights reserved.
+//
+
+#ifndef Event_h
+#define Event_h
+
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+class Event
+{
+public:
+    
+    Event(bool initial, bool manual) :
+    state(initial), manual(manual)
+    {
+        
+    }
+    
+    void Signal()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        
+        if (state)
+            return;
+        
+        state = true;
+        if (manual)
+            condition.notify_all();
+        else
+            condition.notify_one();
+    }
+    
+    void Clear()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        
+        state = false;
+    }
+    
+    void Wait()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        
+        condition.wait(lock, [this] { return state; });
+        
+        if (!manual)
+            state = false;
+    }
+    
+    template<class Rep, class Period>
+    void Wait(const std::chrono::duration<Rep, Period> &timeout)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        
+        if (!condition.wait_for(lock, timeout, [this] { return state; }))
+            return;
+        
+        if (!manual)
+            state = false;
+    }
+    
+private:
+    
+    std::mutex mutex;
+    std::condition_variable condition;
+    bool state, manual;
+};
+
+#endif /* Event_h */
